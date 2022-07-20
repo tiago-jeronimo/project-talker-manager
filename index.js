2e83a9a0a744499a7fe3d7d39d7a96976c36a169
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-07-20 10:43:33 -0300

    feat: Requisito 5 - Feito

--- index.js
+++ index.js
@@ -69,9 +69,9 @@ app.get('/talker/:id', req2);
 //
 
 //
-// REQUISITO 3
+// REQUISITO 3 & 4
 //
-//  MIDDLEWARE REQUISITO 3 
+//  MIDDLEWARE REQUISITO 3 & 4
 const req3 = (req, res, next) => {
   const { email, password } = req.body;
   const regex = /^[\w.]+@([\w-]+\.)+[\w-]{2,4}$/g;
@@ -91,6 +91,93 @@ app.post('/login', req3, (_req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   res.status(200).json({ token });
 });
+//
+//
+//
+
+// REQUISITO5 
+//
+//  MIDDLEWARES REQUISITO 5
+const req5TokenValidation = (req, res, next) => {
+    const { authorization } = req.headers;
+      if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+      if (authorization.length < 16) return res.status(401).json({ message: 'Token inválido' });
+   next();
+};
+
+const req5NameValidation = (req, res, next) => {
+    const { name } = req.body;
+     if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+     if (name.length < 3) {
+      return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+       }
+       next();
+};
+
+const req5AgeValidation = (req, res, next) => {
+  const { age } = req.body;
+   if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+   if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+     }
+     next();
+};
+
+const req5TalkValidation = (req, res, next) => {
+  const { talk } = req.body;
+   if (!talk) return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+     next();
+};
+
+const req5watchedAtValidation = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+   if (!watchedAt) return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+   const regex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+   if (!regex.test(watchedAt)) {
+    return res.status(400)
+    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' }); 
+}
+     next();
+};
+const req5RateValidation = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+   if (!rate && rate !== 0) {
+ return res.status(400)
+   .json({ message: 'O campo "rate" é obrigatório' }); 
+}
+  if (rate < 1 || rate > 5) {
+    return res.status(400)
+    .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+     next();
+};
+
+async function escrita(param) {
+  await fs.writeFile('./talker.json', JSON.stringify(param));
+}
+
+async function req5(req, res) {
+  const data = await readFile();
+  const { name, age, talk: { watchedAt, rate } } = req.body;
+  data.push({ id: data.length + 1, name, age, talk: { watchedAt, rate } });
+  await escrita(data);
+  res.status(201).json({
+    id: data.length,
+    name,
+    age,
+    talk: { watchedAt, rate },
+  });
+}
+//
+// ROTA
+app.post('/talker', 
+        req5TokenValidation,
+        req5NameValidation,
+        req5AgeValidation,
+        req5TalkValidation,
+        req5watchedAtValidation,
+        req5RateValidation, 
+        req5);
 //
 //
 //
