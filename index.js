00c1392e7fcf0e23af140d83a5da6971dc804a34
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-07-20 09:15:16 -0300

    feat: Requisito 1 - Feito

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,6 +13,38 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+//
+//
+// REQUISITO 1
+//
+//
+// FUNÇÃO QUE LÊ
+async function readFile() {
+  try {
+    const data = await fs.readFile('./talker.json', 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+//
+//
+// 
+//  MIDDLEWARE REQUISITO 1
+const req1 = async (_req, res) => {
+  try {
+    const data = await readFile();
+    if (!data) return res.status(200).json([]);
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+//
+// ROTA
+app.get('/talker', req1);
+//
+//
 app.listen(PORT, () => {
   console.log('Online');
 });
